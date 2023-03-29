64c962489208e03df431498f91868f24bd8a7251
rehaman <rehaman047@gmail.com>
2023-03-29 17:06:06 +0530

    Email 1

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,38 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(this.getPassword().equals(oldPassword))
+        {
+            int n=newPassword.length();
+            if(n>=8)
+            {
+                boolean upper=false;boolean lower=false;boolean digit=false;boolean special=false;
+                for(int i=0;i<n;i++)
+                {
+                    if(newPassword.charAt(i)>=65&&newPassword.charAt(i)<=90)
+                    {
+                      upper=true;
+                    }
+                    else if(newPassword.charAt(i)>=97&&newPassword.charAt(i)<=122)
+                    {
+                        lower=true;
+                    }
+                    else if(newPassword.charAt(i)>=48&&newPassword.charAt(i)<=57)
+                    {
+                        digit=true;
+                    }
+                    else
+                    {
+                        special=true;
+                    }
+                }
+                if(upper&&lower&&digit&&special)
+                {
+                   this.password=newPassword;
+                }
+            }
+        }
+
+
     }
 }
